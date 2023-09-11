e11656cf06e45fc2f2495759bbe3a9a1e554b924
Dworv YT <dwarvyt@gmail.com>
2023-09-11 14:32:45 -0700

    feat: user level and syscalls

--- src/interrupts-c.c
+++ src/interrupts-c.c
@@ -26,16 +26,54 @@ void int_init(void) {
         dst[i] = src[i];
 }
 
+void interrupt_vector(unsigned pc) {
+    dev_barrier();
+
+    printk("just got an interrupt!");
+}
+
+static inline uint32_t cpsr_get(void) {
+    uint32_t cpsr;
+    asm volatile("mrs %0,cpsr" : "=r"(cpsr));
+    return cpsr;
+}
+
+// this is used for syscalls.
+int software_interrupt_vector(unsigned pc, uint32_t r0) {
+	uint32_t inst, sys_num, mode;
+    sys_num = r0;
+    uint32_t spsr;
+    asm volatile("mrs %0,spsr" : "=r"(spsr));
+
+    mode = spsr & (uint32_t)((1 << 5)-1);
+
+    // put spsr mode in <mode>
+    if(mode == 0b10000)
+        panic("mode = %b: expected %b\n", mode, 0b10000);
+    else
+        trace("success: spsr is at user level\n");
+
+    // we do a very trivial hello and exit just to show the difference
+    switch(sys_num) {
+    case 1: 
+            trace("syscall: hello world\n");
+            return 0;
+    case 2: 
+            trace("exiting!\n");
+            clean_reboot();
+    default: 
+            printk("illegal system call = %d!\n", sys_num);
+            return -1;
+    }
+}
+
 #define UNHANDLED(msg,r) \
 	panic("ERROR: unhandled exception <%s> at PC=%x\n", msg,r)
+
 void fast_interrupt_vector(unsigned pc) {
 	UNHANDLED("fast", pc);
 }
 
-// this is used for syscalls.
-void software_interrupt_vector(unsigned pc) {
-	UNHANDLED("soft interrupt", pc);
-}
 void reset_vector(unsigned pc) {
 	UNHANDLED("reset vector", pc);
 }
--- src/kernel.c
+++ src/kernel.c
@@ -10,30 +10,23 @@ const int led = 20;
 const int flasher = 21;
 const int input = 16;
 
-void interrupt_vector(unsigned pc) {
-    dev_barrier();
+void run_user_code_asm(void (*fn)(void), void *stack);
+void syscall_hello();
 
-    unsigned pending = GET32(IRQ_basic_pending);
-
-    // if this isn't true, could be a GPU interrupt (as discussed in Broadcom):
-    // just return.  [confusing, since we didn't enable!]
-    if((pending & RPI_BASIC_ARM_TIMER_IRQ)) {
-        PUT32(arm_timer_IRQClear, 1);
-        dev_barrier();
-        status ^= 1;
-        gpio_write(21, status);
-        printk("flipped\n");
-    }
-
-    if(gpio_event_detected(16)) {
-        if(gpio_read(input))
-            gpio_set_on(led);
-        else
-            gpio_set_off(led);
-        gpio_event_clear(16);
-    }
+void user_code(void) {
+    printk("entered user code\n");
+    printk("calling syscall\n");
+    syscall_hello();
+    printk("done syscall\n");
 }
 
+void run_user_code(void (*fn)(void), void *stack) {
+    assert(stack);
+    demand((unsigned)stack % 8 == 0, "stack must be 8 byte aligned\n");
+
+    run_user_code_asm(fn, stack);
+    not_reached();
+}
 
 void notmain(void) {
     uart_init();
@@ -41,24 +34,14 @@ void notmain(void) {
     printk("about to install handlers\n");
     int_init();
 
-    printk("setting up timer interrupts\n");
-    // Q: if you change 0x100?
-    timer_interrupt_init(0x100000);
-
     printk("gonna enable ints globally!\n");
     // Q: what happens (&why) if you don't do?
     system_enable_interrupts();
     printk("enabled!\n");
 
-    gpio_set_output(led);
-    gpio_set_output(flasher);
-    gpio_int_rising_edge(input);
-    gpio_int_falling_edge(input);
-
-    while(1) {
-        // if(gpio_read(input))
-        //     gpio_set_on(led);
-        // else
-        //     gpio_set_off(led);
-    }
+    printk("running user code\n");
+    uint64_t *sp = (uint64_t*)500000;
+    run_user_code(user_code, sp);
+    printk("doone");
+    clean_reboot();
 }
